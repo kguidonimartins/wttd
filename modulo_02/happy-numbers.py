4229e6fcfdb3111d3d0f7f89590a18154d127943
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2019-08-16 17:38:18 -0300

    WIP add happy number

---
+++ modulo_02/happy-numbers.py
@@ -0,0 +1,16 @@
+def happy(number):
+    if number in (1, 10, 100):
+        string = str(number)
+
+        digits = []
+        total = 0
+        for char in string:
+            total += int(char)
+
+        return total == 1
+
+
+assert happy(1)
+assert happy(10)
+assert happy(100)
+assert not happy(4)
--- modulo_02/m2a09_dojo_gameplay_happy_numbers.md
+++ modulo_02/m2a09_dojo_gameplay_happy_numbers.md
@@ -1,1 +1,5 @@
 # M2A09: Dojo Gameplay Happy Numbers - https://welcometothedjango.com.br/?post_type=aula&p=1105
+
+TODO: Rever vídeo happy number e documentar processo TDD
+
+ver `happy-numbers.py`
